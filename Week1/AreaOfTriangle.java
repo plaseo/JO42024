e70606ecb37a9e6176688ddd502010695f007be8
cameron@plaseo.org <cameron@camerons-MacBook-Pro.local>
2024-05-19 18:44:38 -0500

    Update area of triangle to a method prompting user for height and base-width

--- Week1/AreaOfTriangle.java
+++ Week1/AreaOfTriangle.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class AreaOfTriangle {
     public static void main (String[] args){
    Scanner input = new Scanner(System.in);
-   System.out.println("Enter the base-width of triangble: ");
+   System.out.print("Enter the base-width of triangble: ");
    double baseWidth = input.nextDouble();
-   System.out.println("Enter the height of triangle: ");
+   System.out.print("Enter the height of triangle: ");
    double heightTriangle = input.nextDouble();
    getAreaOfTriangle(baseWidth, heightTriangle);
    input.close();
@@ -13,7 +13,7 @@ public class AreaOfTriangle {
     //Method to prmpt the user for base-width and height of triangle, then calculate area
     private static double getAreaOfTriangle(double baseWidth, double heightTriangle) {
     double areaOfTriangle = .5 * baseWidth * heightTriangle;
-    System.out.println(areaOfTriangle);
+    System.out.println("The area of the triangle is: " + areaOfTriangle);
     return areaOfTriangle;
 
     }
