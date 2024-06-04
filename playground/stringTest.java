6c2597f00c7d0ecb0d96cdf5f862dcddefa2376b
cameron@plaseo.org <cameron@plaseo.org>
2024-06-04 15:48:24 -0500

    got some pictures for doggie website update notes for Java

---
+++ playground/stringTest.java
@@ -0,0 +1,12 @@
+package playground;
+
+public class stringTest {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+
+
+    }
+    
+}
