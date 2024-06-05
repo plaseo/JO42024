1cc83c05b8256a5a58afad84dcd2160acbe47a14
cameron@plaseo.org <cameron@plaseo.org>
2024-06-05 08:15:08 -0500

    springboot more

---
+++ Week4/SpringTest/plaseo/src/main/java/com/plaseo/plaseo/PlaseoApplication.java
@@ -0,0 +1,13 @@
+package com.plaseo.plaseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaseoApplication.class, args);
+	}
+
+}
---
+++ Week4/SpringTest/plaseo/src/test/java/com/plaseo/plaseo/PlaseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plaseo.plaseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
